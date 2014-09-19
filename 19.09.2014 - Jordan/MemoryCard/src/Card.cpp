43f3d288a432eac5530ddab8ce6d615dc495837d
Jordan Mitarov <jordan.mitarov@gmail.com>
2014-09-19 19:46:22 +0300

    Add SoundsEffect and WorkMuteButton
    SoundEffect-ClickCard ,MusicPlayGame and Work MuteMusic Button

---
+++ 19.09.2014 - Jordan/MemoryCard/src/Card.cpp
@@ -0,0 +1,20 @@
+/*
+ * Card.cpp
+ *
+ *  Created on: Sep 16, 2014
+ *      Author: Todor
+ */
+
+#include "Card.h"
+
+Card::Card() {
+	// TODO Auto-generated constructor stub
+	int face = 0;
+	int suit = 0;
+	int state = 0;
+}
+
+Card::~Card() {
+	// TODO Auto-generated destructor stub
+}
+
