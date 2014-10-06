1aa095fa4194f38d84613f4591374e09b03ba93a
TSpasov <todorspasoff@hotmail.com>
2014-10-06 11:09:56 +0300

    timers

---
+++ src_11_7_Timer/Statistics.h
@@ -0,0 +1,17 @@
+/*
+ * Statistics.h
+ *
+ *  Created on: Oct 2, 2014
+ *      Author: User
+ */
+
+#ifndef STATISTICS_H_
+#define STATISTICS_H_
+
+class Statistics {
+public:
+	Statistics();
+	virtual ~Statistics();
+};
+
+#endif /* STATISTICS_H_ */
