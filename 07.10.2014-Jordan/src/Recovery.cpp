430cfb7c2df86d2ecebcae4d794a961d2ad3e33b
Jordan Mitarov <jordan.mitarov@gmail.com>
2014-10-07 10:34:42 +0300

    Controm Button in Game
    1.Button Menu,win,bet,play in Game
    2.New background
    3.Sounds for timer less than 4 sec

---
+++ 07.10.2014-Jordan/src/Recovery.cpp
@@ -0,0 +1,18 @@
+/*
+ * Recovery.cpp
+ *
+ *  Created on: Oct 2, 2014
+ *      Author: User
+ */
+
+#include "Recovery.h"
+
+Recovery::Recovery() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Recovery::~Recovery() {
+	// TODO Auto-generated destructor stub
+}
+
