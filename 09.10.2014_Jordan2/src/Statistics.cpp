cc958e5f78b6b499b257fee0c3555169c5b9c100
Jordan Mitarov <jordan.mitarov@gmail.com>
2014-10-09 20:21:30 +0300

    GAME UPDATE BET
    1. Променено е потребителя да изибра колко залог иска да заложи.
    2. Потребителя може само да залага,не повече от неговия кредит и не
    по-малко от пет кредита.

---
+++ 09.10.2014_Jordan2/src/Statistics.cpp
@@ -0,0 +1,149 @@
+#include "Statistics.h"
+
+Statistics::Statistics() {
+	// TODO Auto-generated constructor stub
+	credit = 50;
+	bet = 5;
+	profit = 0;
+	clickForLevel = 0;
+	secondToEnd = 0;
+	gameLevel = 0;
+
+}
+
+Statistics::~Statistics() {
+	// TODO Auto-generated destructor stub
+}
+
+int Statistics::getSecondToEnd() const {
+	return secondToEnd;
+}
+
+void Statistics::setSecondToEnd(int secondToEnd) {
+	this->secondToEnd = secondToEnd;
+}
+
+void Statistics::setClickForLevel(int clickForLevel) {
+	this->clickForLevel = clickForLevel;
+}
+
+int Statistics::getClickForLevel() const {
+	return clickForLevel;
+}
+
+void Statistics::calculateProfit() {
+
+	if (secondToEnd == 0) {
+		profit = - bet;
+	} else {
+		profit =
+				((bet * secondToEnd) - (((gameLevel + 2) * 2) - clickForLevel));
+	}
+		credit += profit;
+}
+
+//((gameLevel + 3) * 2) - clickForLevel;
+
+void Statistics::setGameLevel(int gameLevel) {
+	this->gameLevel = gameLevel;
+}
+
+int Statistics::getGameLevel() const {
+	return gameLevel;
+}
+
+int Statistics::getProfit() const {
+	return profit;
+}
+
+int Statistics::getCredit() const {
+	return credit;
+}
+
+void Statistics::setCredit(int credit) {
+	this->credit = credit;
+}
+
+void Statistics::setBet(int bets)
+{
+	if(bet >= 5)
+	{
+		this-> bet = bet + bets;
+	}
+
+}
+
+int Statistics::getBet() const
+{
+	return bet;
+}
+
+void Statistics::saveToFile() {
+	Statistics stat;
+	vector<Statistics> statistics;
+
+	fstream File("files/statistics.dat", ios::in | ios::binary);
+	if (File.is_open()) {
+
+		while (!File.eof()) {
+			File.read(reinterpret_cast<char*>(&stat), sizeof(Statistics));
+			if (!File.eof()) {
+				statistics.push_back(stat);
+			}
+		}
+	}
+	File.close();
+	if (statistics.size() > 9) {
+		statistics.erase(statistics.begin());
+	}
+	statistics.push_back(*this);
+	ofstream newFile("files/statistics.txt", ios::binary | ios::out);
+//	if (newFile.is_open()) {
+//		for (unsigned int i = 0; i < statistics.size() - 1; i++) {
+//			newFile.write()
+//		}
+//	}
+
+	newFile.close();
+
+	/*	ofstream File("files/statistics.dat", ios::app | ios::binary);
+	 if (File.is_open()) {
+
+	 File.write(reinterpret_cast<char*>(this), sizeof(*this));
+
+	 }
+	 File.close();*/
+
+}
+
+void Statistics::createFile() {
+	fstream File("files/statistics.dat", ios::in | ios::binary);
+}
+
+void Statistics::readFromFile() {
+	Statistics stat;
+	vector<Statistics> statistics;
+
+	fstream File("files/statistics.dat", ios::in | ios::binary);
+	if (File.is_open()) {
+
+		while (!File.eof()) {
+			File.read(reinterpret_cast<char*>(&stat), sizeof(Statistics));
+			if (!File.eof()) {
+				statistics.push_back(stat);
+			}
+		}
+	}
+	File.close();
+//	if (statistics.size() > 9) {
+//		statistics.erase(statistics.begin());
+//	}
+//	for (unsigned i = 0; i < statistics.size(); i++) {
+//		cout << "size" << statistics.size() << endl << endl;
+//		cout << statistics[i].bet << endl;
+//		cout << statistics[i].clickForLevel << endl;
+//		cout << statistics[i].credit << endl;
+//		cout << statistics[i].profit << endl;
+//
+//	}
+}
