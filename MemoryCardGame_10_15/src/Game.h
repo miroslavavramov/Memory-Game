c6ab9516f30c70224a89644fc4fd25e45974bacc
TSpasov <todorspasoff@hotmail.com>
2014-10-15 14:15:32 +0300

    PochtoGotovo

---
+++ MemoryCardGame_10_15/src/Game.h
@@ -0,0 +1,78 @@
+/*
+ * Game.h
+ *
+ *  Created on: Sep 27, 2014
+ *      Author: Todor
+ */
+
+#ifndef GAME_H_
+#define GAME_H_
+
+#include <string>
+#include <iostream>
+#include <ctime>
+#include <set>
+#include "DeckOfCards.h"
+#include "BackgroundTexture.h"
+#include "StartBackgroundTexture.h"
+#include "logicCards.h"
+#include "Buttons.h"
+#include "Timer.h"
+#include "Text.h"
+#include "Card.h"
+#include "Statistics.h"
+#include "Recovery.h"
+
+using namespace std;
+
+class Game {
+	friend class Buttons;
+public:
+	Game();
+	virtual ~Game();
+//	void Init(SDL_Renderer*);
+
+	bool Init(const char*, int, int, int, int, int);
+//	void InitMenu(gameStates);
+	void drawMenu();
+	void updateMenu();
+	void Draw();
+	void Update();
+	void GetCardFromDeck();
+	bool Running();
+	void compareCard();
+
+private:
+	SDL_Window* m_pWindow;
+	SDL_Renderer* m_pRenderer;
+
+	DeckOfCards m_newDeck;
+	Card m_temp;
+
+	Buttons m_buttonsMenu;
+	vector<Buttons> m_buttonSet;
+	Buttons PlayButton;
+
+	BackgroundTexture texture;
+	logicCards m_cardLogic;
+	StartBackgroundTexture StartMenu;
+	SDL_Event e;
+	bool m_bRunning;
+	Timer m_Timer;
+
+	int m_GameLevel;
+	int m_RightGuesses;
+	Statistics m_stat;
+
+	Text txt;
+	Text level;
+
+	Text bet;
+	Text credit;
+	Text profit;
+
+	Recovery m_Recovery;
+
+};
+
+#endif /* GAME_H_ */
---
+++ MemoryCardGame_10_15/src/StartBackgroundTexture.cpp
@@ -0,0 +1,72 @@
+/*
+ * StartBackgroundTexture.cpp
+ *
+ *  Created on: Sep 30, 2014
+ *      Author: User
+ */
+
+#include "StartBackgroundTexture.h"
+
+StartBackgroundTexture::StartBackgroundTexture() {
+	// TODO Auto-generated constructor stub
+
+}
+
+StartBackgroundTexture::~StartBackgroundTexture() {
+	// TODO Auto-generated destructor stub
+}
+
+void StartBackgroundTexture::Init(SDL_Renderer* a) {
+	SDL_Surface* background = IMG_Load("images/menu.png");
+	m_textBackground[0] = SDL_CreateTextureFromSurface(a, background);
+
+	SDL_FreeSurface(background);
+
+}
+
+
+
+void StartBackgroundTexture::InitRules(SDL_Renderer* a) {
+	SDL_Surface* background = IMG_Load("images/rules.png");
+	m_textTeam = SDL_CreateTextureFromSurface(a, background);
+
+	SDL_FreeSurface(background);
+}
+
+void StartBackgroundTexture::Draw(SDL_Renderer* a) {
+
+
+	SDL_RenderCopy(a, m_textBackground[0], 0, 0);
+}
+
+
+
+void StartBackgroundTexture::DrawRules(SDL_Renderer* a) {
+	SDL_RenderCopy(a, m_textTeam, 0, 0);
+
+}
+
+
+void StartBackgroundTexture::InitSatistics(SDL_Renderer*) {
+
+}
+
+
+void StartBackgroundTexture::DrawSatistics(SDL_Renderer* a) {
+
+vector <string> temp;
+
+temp = m_statRecord.readFromFile();
+	m_txtStatistics.setTextColor(0,0,0, 255);
+
+int offset = 20;
+
+	for (int i = 0; i < temp.size(); i ++)
+	{
+		offset += 30;
+		m_txtStatistics.Set(50,50 + offset, 12, 20, temp.at(i));
+		m_txtStatistics.UpdateTextMessage(temp.at(i));
+		m_txtStatistics.Draw(a);
+	}
+
+}
